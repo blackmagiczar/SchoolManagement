3841ff682f182d3dbfcad01478be7baad698178f
blackmagiczar <79756212+blackmagiczar@users.noreply.github.com>
2022-01-01 18:47:37 +0200

    Index file from philani88

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+	  <meta charset = "utf-8">
+	  <link href = "indexStyle.css" rel = "stylesheet">
+	</head>
+	<body>
+	  <table class = "LoginIndex-interface-center">
+	    <div class = "LoginIndex-header">
+		  <h2>Welcome to .......</h2><hr/>
+		</div>
+		  <tr>
+		    <td><a href = "Login-form.php">LOGIN</a></td>
+		  </tr>
+		  <tr>
+		    <td><a href = "registration.php">CREATE PROFILE</a></td>
+		  </tr>
+		  <tr>
+		    <td><a href = "Website-info.html">Wepsite Information</a></td>
+		  </tr>
+		</div>
+	  </table><hr/>
+	</body><hr/>
+  </html>
